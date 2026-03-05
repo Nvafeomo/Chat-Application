8dff2cc9a212dde1185bbade7b9628f0df9653eb
Nvafeomo Konneh <68853000+Nvafeomo@users.noreply.github.com>
2026-03-05 16:15:24 -0500

    Actual Client program. accidentally had server program in client

--- client.c
+++ client.c
@@ -1,154 +1,159 @@
+
+/* Communication class for project
+This will be what talks to both the server and the client (GUI) */
+
 #include <string.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/tcp.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <pthread.h>
 
-/// Pre-defined default port number for the server.
+/// Server's default port number
 #define SERV_TCP_PORT 23
 
-/// Maximum nubmer of bytes for a message.
+/// Maximum size of a message that can be sent or received.
 #define MAX_SIZE 1024
 
-/// Maximum number of clients that can be connected to the server.
-#define MAX_CLIENTS 5
+pthread_t t1;
+pthread_t t2;
 
-/// Array to store the file descriptors of connected clients. Each index holds a client's socket descriptor or 0 if the slot is available.
-int client_sockets[MAX_CLIENTS];
-
-/// Mutex to ensure thread-safe access to the shared `client_sockets` array and prevent race conditions.
-pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-/// Broadcasts message to all connected clients.
-void broadcast_message(char *message, int sender_socket) {
-    int ret;
-    ret = pthread_mutex_lock(&clients_mutex);
-    if (ret != 0)
-    {
-         perror("pthread_mutex_lock");
-    }
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] != 0 && client_sockets[i] != sender_socket) {
-            send(client_sockets[i], message, strlen(message), 0);
-        }
-    }
-    pthread_mutex_unlock(&clients_mutex);
-    if (ret != 0)
-    {
-         perror("pthread_mutex_unlock");
-    }
-}
-
-/// Handles communication with a client program.
-void *handle_client(void *arg) {
-    int client_socket = *(int *)arg;
-    char buffer[MAX_SIZE];
-    int len;
-
-    while ((len = read(client_socket, buffer, MAX_SIZE)) > 0) {
-        buffer[len] = '\0'; /* Ensure proper string */
-        broadcast_message(buffer, client_socket); /* Broadcast the message */
-    }
-
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] == client_socket) {
-            client_sockets[i] = 0;
-            break;
-        }
-    }
-    pthread_mutex_unlock(&clients_mutex);
-    close(client_socket);
-    return NULL;
-}
-
-/// Initializes the server, listens for incoming connections, and creates threads to handle client communications. Command-line format: server [port_number].
-int main(int argc, char *argv[])
+///Reads messages from the server and continuously listens for incoming messages and prints them to the terminal.
+void *readFromServer(void *fileDescriptor)
 {
-    int sockfd, newsockfd, clilen;
-    struct sockaddr_in cli_addr, serv_addr;
-    int port;
     char string[MAX_SIZE];
     int len;
 
-    /* command line: server [port_number] */
+    int sockfd = * (int *) fileDescriptor;
 
-    if(argc == 2)
-     sscanf(argv[1], "%d", &port); /* read the port number if provided */
-    else
-     port = SERV_TCP_PORT;
-
-    /* open a TCP socket (an Internet stream socket) */
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-     perror("can't open stream socket");
-     exit(1);
-    }
-
-    /* bind the local address, so that the client can send to server */
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(port);
-
-    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-     perror("can't bind local address");
-     exit(1);
-    }
-
-    /* listen to the socket */
-    listen(sockfd, MAX_CLIENTS);
-    printf("\nServer is running!\n");
-
-    /* Initialize client sockets array using a loop */
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        client_sockets[i] = 0;
-    }
-    for(;;) {
-     int ret1;
-
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
-     if(newsockfd < 0) {
-        perror("can't bind local address");
-        continue;
-     }
-
-     int client_sock = newsockfd;
-
-    ret1 = pthread_mutex_lock(&clients_mutex);
-    if (ret1 != 0)
+    for (;;)
     {
-         perror("pthread_mutex_lock");
-    }
-
-    int client_added = 0;
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] == 0) {
-            client_sockets[i] = newsockfd;
-            pthread_t thread_id;
-            pthread_create(&thread_id, NULL, handle_client, &client_sock);
-            pthread_detach(thread_id);
-            client_added = 1;
-            break;
-            }
-    }
-    if (client_added == 0){
-        char *message = "-1";
-        send(newsockfd, message, strlen(message), 0);
-        close(newsockfd);
-    }
-    ret1 = pthread_mutex_unlock(&clients_mutex);
-    if (ret1 != 0)
-    {
-         perror("pthread_mutex_unlock");
-    }
-
+        // Read message from server
+        len = read(sockfd, string, MAX_SIZE);
+        /* make sure it's a proper string */
+        string[len] = 0;
+        printf("%s\n", string);
     }
     close(sockfd);
-    return 0;
+    pthread_exit(0);
+}
+
+///Reads messages from the server.
+///Retrieves username and continuously prompts the user for messages to be sent from client and sends the messages to the server using write().
+void *sendMessageToServer(void *fileDescriptor)
+{
+    int sockfd = * (int *) fileDescriptor;
+
+    // Client joins the chat room.
+    char *username;
+    size_t usernameLength = 0;
+    printf("Enter your username: \n");
+    ssize_t nameCount = getline(&username, &usernameLength, stdin);
+    username[nameCount-1] = 0;  // remove the new line from the user name.
+
+    // Get the message to send
+    char *msg;
+    size_t msgCount = 0;
+    char buffer[MAX_SIZE];
+    printf("Type your messages below (type exit to quit):\n");
+    for (;;)
+    {
+        //printf("Type your message (type exit to quit):\n");
+        ssize_t newMsgCount = getline(&msg, &msgCount, stdin);
+        msg[newMsgCount-1] = 0;
+
+        sprintf(buffer, "%s: %s", username, msg);
+
+        if (newMsgCount > 0)
+        {
+            if (strcmp(msg,"exit")==0)
+                break;
+
+            // Write a message to the server
+            write(sockfd, buffer, sizeof(buffer));
+        }
+    }
+    pthread_cancel(t1);
+    pthread_exit(0);
+}
+
+/// Initializes the client, connects to the server,
+/// and creates threads for message sending and receiving.
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    char *serv_host = "localhost";
+    struct hostent *host_ptr;
+    int port;
+    int buff_size = 0;
+
+    //char string[MAX_SIZE];
+    //int len;
+
+    /* command line: client [host [port]] */
+
+    if(argc >= 2)
+        serv_host = argv[1];  // read the host if provided
+
+    if (argc == 3)
+        sscanf(argv[2], "%d", &port);  // read the port number if provided
+    else
+        port = SERV_TCP_PORT;
+
+    // Get the address of the host
+    if ( (host_ptr = gethostbyname(serv_host)) == NULL)
+    {
+        perror("gethostbyname error");
+        exit(1);
+    }
+
+    // Check if the address type is AF_INET (IPv4 Internet protocol)
+    if(host_ptr->h_addrtype !=  AF_INET) {
+        perror("unknown address type");
+        exit(1);
+    }
+
+    // Erase any data about the server address
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr =
+    ((struct in_addr *)host_ptr->h_addr_list[0])->s_addr;
+    serv_addr.sin_port = htons(port);
+
+    // Open a TCP socket (Just like the server)
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("can't open stream socket");
+        exit(1);
+    }
+
+    // Connect to the server
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("cannot connect to the server.");
+        exit(1);
+    }
+
+    // Thread for receiving messages from the server.
+    int s;
+    s = pthread_create(&t1, NULL, readFromServer, &sockfd);
+    if (s != 0)
+    {
+        perror("Client read error.");
+    }
+
+    // Thread for sending messages to the server
+    s = pthread_create(&t2, NULL, sendMessageToServer, &sockfd);
+    if (s != 0)
+    {
+        perror("Client send error.");
+    }
+
+    pthread_exit(0);
 }
